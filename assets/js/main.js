fc57311eb361bf6635d963762e5449584c02b6be
Samuel <snipps0233@gmail.com>
2020-10-04 23:36:31 -0400

    - Updated parent screen

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,13 +4,5 @@ $(document).ready(function () {
     var dt = new Date();
     var time = dt.getHours() + ":" + dt.getMinutes();
     $('#current-time p').text(time)
-    $('.shoutouts-container').slick({
-        infinite: true,
-        vertical: true,
-        verticalSwiping: true,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        prevArrow: $('.top-arrow'),
-        nextArrow: $('.bottom-arrow')
-    });
+
 });
